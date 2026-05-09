46218a7cdfce059430ff3cef53b14ab684306f8d
siyarampatidar <patidarsiyaram28@gmail.com>
2026-05-09 15:26:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             "http://localhost:5173",
-            "https://taskmanagementfrontend-es8h.onrender.com"
+            "https://taskmanagerapp-old9.onrender.com"
         ],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         credentials: true
@@ -50,7 +50,7 @@ initSocket(io);
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://taskmanagementfrontend-es8h.onrender.com"
+        "https://taskmanagerapp-old9.onrender.com"
     ],
     credentials: true
 }));
